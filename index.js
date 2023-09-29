967b8d594e0b7fa31da563ba77375987e53c5dc2
ShivdasManwar01 <shivdasmanwar01@gmail.com>
2023-09-29 15:32:17 +0530

    updated

---
+++ index.js
@@ -0,0 +1,14 @@
+// var language_select=document.getElementById("languages");
+// language_select.addEventListener("change",function(){
+//     alert("select changes")
+//     console.log("Shnijvidfdjflkdjlkfjlj"
+//     )
+//     if(language_select.value=="Hindi"){
+//         document.getElementById("title").innerHTML="अनलिमिटेड फ़िल्में, टीवी शो के साथ भी बहुत कुछ"
+//     }
+// })
+var btn=document.getElementById("signin_button_id");
+console.log(btn.innerHTML)
+btn.addEventListener("click",function(){
+    alert("clicked")
+})
